4a44be01a5aed4472d182a5cc5ba5322fd6a502b
yaoming <yaoming@mooeye.cn>
2019-10-23 19:13:37 +0800

    '增加了string-utils,一些常用的字符串处理函数'

---
+++ demo/string-utils.demo.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+source `dirname $0`/../lib/string-utils
+
+echo ">$( STR_GetLeft "a|b|c" "" )<"
+echo ">$( STR_GetLeft "a|b|c" "m" )<"
+echo ">$( STR_GetLeft "a|b|c" "|" )<"
+echo 
+echo ">$( STR_GetLeftOutter "a|b|c" "" )<"
+echo ">$( STR_GetLeftOutter "a|b|c" "m" )<"
+echo ">$( STR_GetLeftOutter "a|b|c" "|" )<"
+echo 
+echo ">$( STR_GetRight "a|b|c" "" )<"
+echo ">$( STR_GetRight "a|b|c" "m" )<"
+echo ">$( STR_GetRight "a|b|c" "|" )<"
+echo 
+echo ">$( STR_GetRightOutter "a|b|c" "" )<"
+echo ">$( STR_GetRightOutter "a|b|c" "m" )<"
+echo ">$( STR_GetRightOutter "a|b|c" "|" )<"
+echo 
+echo ">$( STR_GetSection "a|b|c" "" "" )<"
+echo ">$( STR_GetSection "a|b|c" "m" "x" )<"
+echo ">$( STR_GetSection "a|b|c" "c" "b" )<"
+echo ">$( STR_GetSection "a|b|c" "|" "|" )<"
+echo 
+echo ">$( STR_Trim "\ta|b|c abc      " )<"
+echo ">$( STR_Trim "\ta|b|c abc" "abc" )<"
